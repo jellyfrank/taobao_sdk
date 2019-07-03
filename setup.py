affc34597a94a9fcbbfa3f02cba7cb0ec8fc7751
block-cat <kfx2007@outlook.com>
2019-07-03 09:12:38 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ def read(fname):
 
 from os import path
 this_directory = path.abspath(path.dirname(__file__))
+print('--------')
+print(this_directory)
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
