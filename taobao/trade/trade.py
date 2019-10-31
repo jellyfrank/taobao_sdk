8414659ca76c916172f666f67230a0527876b077
block-cat <kfx2007@outlook.com>
2019-10-31 06:04:36 +0000

    退款查询和关闭订单接口

--- taobao/trade/trade.py
+++ taobao/trade/trade.py
@@ -168,6 +168,35 @@ class TradeApi(Common):
 
         return self.post("taobao.trade.memo.update", data)
 
+    def refunds_receive_get(self, fields, status=None, buyer_nick=None, type=None, start_modified=None,
+                            end_modified=None, page_no=1, page_size=40, use_has_next=False):
+        """
+        查询卖家收到的退款列表
+        参数：
+        fields: 需要返回的字段
+        status: 退款状态，默认查询所有退款状态的数据，除了默认值外每次只能查询一种状态
+        buyer_nick: 卖家昵称
+        type： 交易类型列表，一次查询多种类型可用半角逗号分隔，默认同时查guarantee_trade, auto_delivery这两种类型的数据
+        start_modified： 查询修改时间开始。格式: yyyy-MM-dd HH:mm:ss
+        end_modified： 查询修改时间结束。格式: yyyy-MM-dd HH:mm:ss
+        page_no： 页码。取值范围:大于零的整数; 默认值:1
+        page_size： 每页条数。取值范围:大于零的整数; 默认值:40;最大值:100
+        use_has_next： 是否启用has_next的分页方式，如果指定true,则返回的结果中不包含总记录数,但是会新增一个是否存在下一页的的字段，通过此种方式获取增量退款，接口调用成功率在原有的基础上有所提升
+        """
+        data = {
+            "fields": fields,
+            "status": status,
+            "buyer_nick": buyer_nick,
+            "type": type,
+            "start_modified": start_modified,
+            "end_modified": end_modified,
+            "page_no": page_no,
+            "page_size": page_size,
+            "use_has_next": use_has_next
+        }
+
+        return self.post("taobao.refunds.receive.get", data)
+
     def trade_amount_get(self, fields, tid):
         """
         交易账务查询
@@ -181,21 +210,14 @@ class TradeApi(Common):
 
         return self.post("taobao.trade.amount.get", data)
 
-    def trade_memo_update(self, tid, memo="", flag=0, reset=False):
+    def trade_close(self, tid, close_reason):
         """
-        修改交易备注
-        需要商家或以上权限才可调用此接口，可重复调用本接口更新交易备注，本接口同时具有添加备注的功能
-        参数：
-        tid： 交易编号
-        memo： 交易备注（1000字节）
-        flag： 卖家交易备注旗帜，可选值为：0(灰色), 1(红色), 2(黄色), 3(绿色), 4(蓝色), 5(粉红色)，默认值为0
-        reset： 是否对memo的值置空若为true
+        卖家关闭一笔交易
+        关闭一笔订单，可以是主订单或子订单。当订单从创建到关闭时间小于10s的时候，会报“CLOSE_TRADE_TOO_FAST”错误。
         """
         data = {
             "tid": tid,
-            "memo": memo,
-            "flag": flag,
-            "reset": reset
+            "close_reason": close_reason
         }
 
-        return self.post("taobao.trade.memo.update", data)
+        return self.post("taobao.trade.close", data)
